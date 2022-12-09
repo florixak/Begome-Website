117366fb25d92522dc7d555287aea71b2397434f
florixak <florixakcz@seznam.cz>
2022-12-09 23:23:24 +0100

    update

--- script.js
+++ script.js
@@ -21,8 +21,41 @@ function followersMsg() {
 function schedule() {
     const d = new Date();
     const weekday = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-    let day = d.getDay()
+    var day = d.getDay()
+    var d2 = new Date(d.getFullYear(), d.getMonth(), d.getDay(), 18, 0, 0);
 
-    document.getElementById("monday").innerHTML = weekday[day];
+    let date1 = new Date("06/21/2022").getTime();
+    let date2 = new Date("07/28/2021").getTime();
+    
+    switch (day) {
+        case 0:
+            document.getElementById(weekday[0]).innerHTML = "<b>" + document.getElementById(weekday[0]).outerText + "</b>";
+            // if (d.getTime() >= d2.getTime()) document.getElementById(weekday[0]).innerHTML += " (STREAMING NOW ⚪)";
+            break;
+        case 1:
+            document.getElementById(weekday[1]).innerHTML = "<b>" + document.getElementById(weekday[1]).outerText + "</b>";
+            // if (d.getTime() >= d2.getTime()) document.getElementById(weekday[1]).innerHTML += " (STREAMING NOW ⚪)";
+            break;
+        case 2:
+            document.getElementById(weekday[2]).innerHTML = "<b>" + document.getElementById(weekday[2]).outerText + "</b>";
+            // if (d.getTime() >= d2.getTime()) document.getElementById(weekday[2]).innerHTML += " (STREAMING NOW ⚪)";
+            break;
+        case 3:
+            document.getElementById(weekday[3]).innerHTML = "<b>" + document.getElementById(weekday[3]).outerText + "</b>";
+            // if (d.getTime() >= d2.getTime()) document.getElementById(weekday[3]).innerHTML += " (STREAMING NOW ⚪)";
+            break;
+        case 4:
+            document.getElementById(weekday[4]).innerHTML = "<b>" + document.getElementById(weekday[4]).outerText + "</b>";
+            // if (d.getTime() >= d2.getTime()) document.getElementById(weekday[4]).innerHTML += " (STREAMING NOW ⚪)";
+            break;
+        case 5:
+            document.getElementById(weekday[5]).innerHTML = "<b>" + document.getElementById(weekday[5]).outerText + "</b>";
+            // if (d.getTime() >= d2.getTime()) document.getElementById(weekday[5]).innerHTML += " (STREAMING NOW ⚪)";
+            break;
+        case 6:
+            document.getElementById(weekday[6]).innerHTML = "<b>" + document.getElementById(weekday[6]).outerText + "</b>";
+            // if (d.getTime() >= d2.getTime()) document.getElementById(weekday[6]).innerHTML += " (STREAMING NOW ⚪)";
+            break;
+    }
     
 }
